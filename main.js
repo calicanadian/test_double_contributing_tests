cde588c75cee13afd04217a1090db859adaa5b80
calicanadian <ryan.stuhl@gmail.com>
2019-08-21 20:01:22 -0700

    Final commit of take-home assignment. Added documentation and an index file with basic instructions.

--- main.js
+++ main.js
@@ -18,35 +18,7 @@ let accountNumberTranslation = [
   []
 ];
 
-function validateAccountNumber(accountNumber) {
-  // need to look at how to preserve the leading zeros at the front of the account number
-  // Limitation to this program: Cannot have account numbers with leading 0's
-  if(accountNumber.match(illegalCharactersRegex)) {
-    return String(accountNumber + " ILL");
-  }
-  let accountNumberArray = splitAccountNumber(accountNumber);
-  let accountNumberValid = false;
-  accountNumberValid = checkSumAccountNumber(accountNumberArray);
-  if(accountNumberValid) {
-    return accountNumber;
-  } else {
-    return String(accountNumber + " ERR");
-  }
-}
-
-function checkSumAccounNumber(acct) {
-  let acctLen = acct.length - 1;
-  checksum = ((acct[acctLen] * 3) + (acct[acctLen - 1] * 7) + (acct[acctLen - 2] * 1) + (acct[acctLen - 3] * 3) + (acct[acctLen - 4] * 7) + (acct[acctLen - 5] * 1) + (acct[acctLen - 6] * 3) + (acct[acctLen - 7] * 7) + (acct[acctLen - 8] * 1))%11 == 0;
-  return checksum;
-}
-
-function splitAccountNumber(accountNumber) {
-  // take the number, split it as a string, parse each number and map them into a new array.
-  let numberArray = (accountNumber).toString(10).split("").map(function(num){ return parseInt(num) });
-  // return the new array of numbers.
-  return numberArray;
-}
-
+// ####### Story 1 Scan account number and digest
 // recursive function. i = 0..8 or 9 digit account number length.
 // each loop through the function will increment the iterator by 1.
 function accountNumberScan(accountNumber, i=0) {
@@ -74,6 +46,11 @@ function accountNumberScan(accountNumber, i=0) {
 // function to take each number of an account number and digest it into
 // the represented string for translation.
 function digestNumber(number, level, position) {
+  console.dir(number);
+  console.dir(level);
+  console.dir(position);
+  console.dir(numberTranslation);
+  console.dir(accountNumberTranslation);
   // initialize the iterator.
   let i;
   // until the iterator is equal to the length of the numberTranslation (4 - 1) array
@@ -85,4 +62,71 @@ function digestNumber(number, level, position) {
     accountNumberTranslation[position][i] = numberTranslation[level][number - 1];
   }
   return;
+}
+
+// ####### Story 2 Validate and Checksum && Story 3 Print out results
+// ******* Limitation to this program: Cannot have account numbers with leading 0's *******
+function validateAccountNumber(accountNumber) {
+  if(accountNumber.match(illegalCharactersRegex)) {
+    return String(accountNumber + " ILL");
+  } else {
+    let accountNumberArray = splitAccountNumber(accountNumber);
+    let accountNumberValid = false;
+    accountNumberValid = checkSumAccountNumber(accountNumberArray);
+    if(accountNumberValid) {
+      return accountNumber;
+    } else {
+      // return String(accountNumber + " ERR"); // I've commented this out so we can move on to Story 4
+      analyzeAccountError(accountNumberArray);
+    }
+  }
+}
+
+function splitAccountNumber(accountNumber) {
+  // take the number, split it as a string, parse each number and map them into a new array.
+  let numberArray = (accountNumber).toString(10).split("").map(function(num){ return parseInt(num) });
+  // return the new array of numbers.
+  return numberArray;
+}
+
+function checkSumAccounNumber(acct) {
+  let acctLen = acct.length - 1;
+  checksum = ((acct[acctLen] * 3) + (acct[acctLen - 1] * 7) + (acct[acctLen - 2] * 1) + (acct[acctLen - 3] * 3) + (acct[acctLen - 4] * 7) + (acct[acctLen - 5] * 1) + (acct[acctLen - 6] * 3) + (acct[acctLen - 7] * 7) + (acct[acctLen - 8] * 1))%11 == 0;
+  return checksum;
+}
+
+// ####### Story 4 Check if error account numbers were improperly scanned
+
+// if account number returned an error, it means the checkSum failed
+// Need to check each 1, 5, 9, or 0 to see if they should actually be
+// a 7, [6,9], 8, 8. Find the first instance of 1 and change it to a 7,
+// then run the checksum again. If it doesn't return true, rinse and repeat.
+
+function analyzeAccountError(accountNumberArray, i = 0) {
+  if (i <= 3) {
+    // list out all possible matches
+    let possibleMatches = [1, 5, 9, 0];
+    // set the accountNumberArray aside for mutation
+    let acctNumArray = accountNumberArray;
+    let r = null;
+    // loop through the acctNumArray and compare each number to each the possibleMatches[i]
+    for (r = 0; r < (acctNumArray.length - 1); r++) {
+      // if we have a match, run the checkSum against the mutated array
+      if (acctNumArray[r] == possibleMatches[i]) {
+        acctNumArray[r] = possibleMatches[i];
+        if (checkSumAccounNumber(acctNumArray)) {
+          // if checkSum is true, return the mutated account number
+          return acctNumArray.join();
+        }
+      }
+    }
+    // if the botched account number doesn't contain possibleMatches[i], iterate and check the next possbile match.
+    i++;
+    // call the function recursively
+    analyzeAccountError(accountNumberArray, i);
+  } else {
+    // if there were no matches, just return the original account number with ERR
+    return String(accountNumberArray.join() + " ERR");
+  }
+
 }
